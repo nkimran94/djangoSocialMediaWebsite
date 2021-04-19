ecf5a9a2113fefb5800d86baa138d92df3f01667
nkimran94 <nkimranewu@gmail.com>
2021-04-19 14:07:43 +0600

    user registration added

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,17 @@ urlpatterns = [
     #path('login/',views.user_login,name='login'),
     path('login/',auth_views.LoginView.as_view(),name='login'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
+    path('',views.dashboard, name='dashboard'),
+    
+    #change password URLs
+    path('password_change/',auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done',auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
+
+    #reset password urls
+    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset_done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+
+    path('register/',views.register,name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
 
 def user_login(request):
     if request.method == 'POST':
@@ -22,3 +24,22 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request,'account/login.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            #create a new user object but avoid saving it yet.
+            new_user = user_form.save(commit=False)
+            #set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            #save the user object
+            new_user.save()
+            return render(request, 'account/register_done.html',{'new_user':new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html',{'user_form':user_form})
