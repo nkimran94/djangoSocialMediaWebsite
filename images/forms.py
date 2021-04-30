76515b26da845abb0b44022a281e70a7dcb26df9
nkimran94 <nkimranewu@gmail.com>
2021-04-30 23:32:40 +0600

    images added

--- images/forms.py
+++ images/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-form .models import Image
+from .models import Image
 from urllib import request
 from django.core.files.base import ContentFile
 from django.utils.text import slugify
--- images/urls.py
+++ images/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-form . import views
+from . import views
 
 app_name = 'images'
 urlpatterns = [
